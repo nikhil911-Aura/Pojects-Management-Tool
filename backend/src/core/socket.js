3bc5f07b873949787df5e6b433501654a5f8c35b
Nikhil Singh <nikhil.singhibrinfotech@gmail.com>
2026-04-15 11:23:37 +0530

    ip change in code

--- backend/src/core/socket.js
+++ backend/src/core/socket.js
@@ -9,10 +9,10 @@ export const initSocket = (server) => {
       origin: [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-        'http://192.168.1.22:3000',
+        'http://192.168.1.24:3000',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'http://192.168.1.22:5173'
+        'http://192.168.1.24:5173'
       ],
       methods: ['GET', 'POST'],
       credentials: true
