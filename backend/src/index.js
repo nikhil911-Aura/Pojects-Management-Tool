40346d0f47d719c16f1f0eff9bd4815cb4acf16e
Nikhil Singh <nikhil.singhibrinfotech@gmail.com>
2026-05-05 12:31:43 +0530

    setting page fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://192.168.1.26:3000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://172.19.128.1:3000'],
   credentials: true
 }));
 app.use(express.json());
